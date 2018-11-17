d987f87467e88e73db5d31c4a748d645f3fef06c
qingguoguo03 <weiweizhu_nju@163.com>
2018-11-17 11:31:20 +0800

    Add files via upload
    检查论文是否下载完全

---
+++ DTM/final_check.py
@@ -0,0 +1,383 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 09:27:52 2018
+
+@author: Administrator
+"""
+
+isr = '''
+https://pubsonline.informs.org/toc/isre/29/3
+https://pubsonline.informs.org/toc/isre/29/3
+https://pubsonline.informs.org/toc/isre/29/2
+https://pubsonline.informs.org/toc/isre/29/2
+https://pubsonline.informs.org/toc/isre/29/1
+https://pubsonline.informs.org/toc/isre/29/1
+https://pubsonline.informs.org/toc/isre/28/4
+https://pubsonline.informs.org/toc/isre/28/4
+https://pubsonline.informs.org/toc/isre/28/3
+https://pubsonline.informs.org/toc/isre/28/3
+https://pubsonline.informs.org/toc/isre/28/2
+https://pubsonline.informs.org/toc/isre/28/2
+https://pubsonline.informs.org/toc/isre/28/1
+https://pubsonline.informs.org/toc/isre/28/1
+https://pubsonline.informs.org/toc/isre/27/4
+https://pubsonline.informs.org/toc/isre/27/4
+https://pubsonline.informs.org/toc/isre/27/3
+https://pubsonline.informs.org/toc/isre/27/3
+https://pubsonline.informs.org/toc/isre/27/2
+https://pubsonline.informs.org/toc/isre/27/2
+https://pubsonline.informs.org/toc/isre/27/1
+https://pubsonline.informs.org/toc/isre/27/1
+https://pubsonline.informs.org/toc/isre/26/4
+https://pubsonline.informs.org/toc/isre/26/4
+https://pubsonline.informs.org/toc/isre/26/3
+https://pubsonline.informs.org/toc/isre/26/3
+https://pubsonline.informs.org/toc/isre/26/2
+https://pubsonline.informs.org/toc/isre/26/2
+https://pubsonline.informs.org/toc/isre/26/1
+https://pubsonline.informs.org/toc/isre/26/1
+https://pubsonline.informs.org/toc/isre/25/4
+https://pubsonline.informs.org/toc/isre/25/4
+https://pubsonline.informs.org/toc/isre/25/3
+https://pubsonline.informs.org/toc/isre/25/3
+https://pubsonline.informs.org/toc/isre/25/2
+https://pubsonline.informs.org/toc/isre/25/2
+https://pubsonline.informs.org/toc/isre/25/1
+https://pubsonline.informs.org/toc/isre/25/1
+https://pubsonline.informs.org/toc/isre/24/4
+https://pubsonline.informs.org/toc/isre/24/4
+https://pubsonline.informs.org/toc/isre/24/3
+https://pubsonline.informs.org/toc/isre/24/3
+https://pubsonline.informs.org/toc/isre/24/2
+https://pubsonline.informs.org/toc/isre/24/2
+https://pubsonline.informs.org/toc/isre/24/1
+https://pubsonline.informs.org/toc/isre/24/1
+https://pubsonline.informs.org/toc/isre/23/4
+https://pubsonline.informs.org/toc/isre/23/4
+https://pubsonline.informs.org/toc/isre/23/3-part-2
+https://pubsonline.informs.org/toc/isre/23/3-part-2
+https://pubsonline.informs.org/toc/isre/23/3-part-1
+https://pubsonline.informs.org/toc/isre/23/3-part-1
+https://pubsonline.informs.org/toc/isre/23/2
+https://pubsonline.informs.org/toc/isre/23/2
+https://pubsonline.informs.org/toc/isre/23/1
+https://pubsonline.informs.org/toc/isre/23/1
+https://pubsonline.informs.org/toc/isre/22/4
+https://pubsonline.informs.org/toc/isre/22/4
+https://pubsonline.informs.org/toc/isre/22/3
+https://pubsonline.informs.org/toc/isre/22/3
+https://pubsonline.informs.org/toc/isre/22/2
+https://pubsonline.informs.org/toc/isre/22/2
+https://pubsonline.informs.org/toc/isre/22/1
+https://pubsonline.informs.org/toc/isre/22/1
+https://pubsonline.informs.org/toc/isre/21/4
+https://pubsonline.informs.org/toc/isre/21/4
+https://pubsonline.informs.org/toc/isre/21/3
+https://pubsonline.informs.org/toc/isre/21/3
+https://pubsonline.informs.org/toc/isre/21/2
+https://pubsonline.informs.org/toc/isre/21/2
+https://pubsonline.informs.org/toc/isre/21/1
+https://pubsonline.informs.org/toc/isre/21/1
+https://pubsonline.informs.org/toc/isre/20/4
+https://pubsonline.informs.org/toc/isre/20/4
+https://pubsonline.informs.org/toc/isre/20/3
+https://pubsonline.informs.org/toc/isre/20/3
+https://pubsonline.informs.org/toc/isre/20/2
+https://pubsonline.informs.org/toc/isre/20/2
+https://pubsonline.informs.org/toc/isre/20/1
+https://pubsonline.informs.org/toc/isre/20/1
+https://pubsonline.informs.org/toc/isre/19/4
+https://pubsonline.informs.org/toc/isre/19/4
+https://pubsonline.informs.org/toc/isre/19/3
+https://pubsonline.informs.org/toc/isre/19/3
+https://pubsonline.informs.org/toc/isre/19/2
+https://pubsonline.informs.org/toc/isre/19/2
+https://pubsonline.informs.org/toc/isre/19/1
+https://pubsonline.informs.org/toc/isre/19/1
+https://pubsonline.informs.org/toc/isre/18/4
+https://pubsonline.informs.org/toc/isre/18/4
+https://pubsonline.informs.org/toc/isre/18/3
+https://pubsonline.informs.org/toc/isre/18/3
+https://pubsonline.informs.org/toc/isre/18/2
+https://pubsonline.informs.org/toc/isre/18/2
+https://pubsonline.informs.org/toc/isre/18/1
+https://pubsonline.informs.org/toc/isre/18/1
+https://pubsonline.informs.org/toc/isre/17/4
+https://pubsonline.informs.org/toc/isre/17/4
+https://pubsonline.informs.org/toc/isre/17/3
+https://pubsonline.informs.org/toc/isre/17/3
+https://pubsonline.informs.org/toc/isre/17/2
+https://pubsonline.informs.org/toc/isre/17/2
+https://pubsonline.informs.org/toc/isre/17/1
+https://pubsonline.informs.org/toc/isre/17/1
+https://pubsonline.informs.org/toc/isre/16/4
+https://pubsonline.informs.org/toc/isre/16/4
+https://pubsonline.informs.org/toc/isre/16/3
+https://pubsonline.informs.org/toc/isre/16/3
+https://pubsonline.informs.org/toc/isre/16/2
+https://pubsonline.informs.org/toc/isre/16/2
+https://pubsonline.informs.org/toc/isre/16/1
+https://pubsonline.informs.org/toc/isre/16/1
+https://pubsonline.informs.org/toc/isre/15/4
+https://pubsonline.informs.org/toc/isre/15/4
+https://pubsonline.informs.org/toc/isre/15/3
+https://pubsonline.informs.org/toc/isre/15/3
+https://pubsonline.informs.org/toc/isre/15/2
+https://pubsonline.informs.org/toc/isre/15/2
+https://pubsonline.informs.org/toc/isre/15/1
+https://pubsonline.informs.org/toc/isre/15/1
+https://pubsonline.informs.org/toc/isre/14/4
+https://pubsonline.informs.org/toc/isre/14/4
+https://pubsonline.informs.org/toc/isre/14/3
+https://pubsonline.informs.org/toc/isre/14/3
+https://pubsonline.informs.org/toc/isre/14/2
+https://pubsonline.informs.org/toc/isre/14/2
+https://pubsonline.informs.org/toc/isre/14/1
+https://pubsonline.informs.org/toc/isre/14/1
+https://pubsonline.informs.org/toc/isre/13/4
+https://pubsonline.informs.org/toc/isre/13/4
+https://pubsonline.informs.org/toc/isre/13/3
+https://pubsonline.informs.org/toc/isre/13/3
+https://pubsonline.informs.org/toc/isre/13/2
+https://pubsonline.informs.org/toc/isre/13/2
+https://pubsonline.informs.org/toc/isre/13/1
+https://pubsonline.informs.org/toc/isre/13/1
+https://pubsonline.informs.org/toc/isre/12/4
+https://pubsonline.informs.org/toc/isre/12/4
+https://pubsonline.informs.org/toc/isre/12/3
+https://pubsonline.informs.org/toc/isre/12/3
+https://pubsonline.informs.org/toc/isre/12/2
+https://pubsonline.informs.org/toc/isre/12/2
+https://pubsonline.informs.org/toc/isre/12/1
+https://pubsonline.informs.org/toc/isre/12/1
+https://pubsonline.informs.org/toc/isre/11/4
+https://pubsonline.informs.org/toc/isre/11/4
+https://pubsonline.informs.org/toc/isre/11/3
+https://pubsonline.informs.org/toc/isre/11/3
+https://pubsonline.informs.org/toc/isre/11/2
+https://pubsonline.informs.org/toc/isre/11/2
+https://pubsonline.informs.org/toc/isre/11/1
+https://pubsonline.informs.org/toc/isre/11/1
+https://pubsonline.informs.org/toc/isre/10/4
+https://pubsonline.informs.org/toc/isre/10/4
+https://pubsonline.informs.org/toc/isre/10/3
+https://pubsonline.informs.org/toc/isre/10/3
+https://pubsonline.informs.org/toc/isre/10/2
+https://pubsonline.informs.org/toc/isre/10/2
+https://pubsonline.informs.org/toc/isre/10/1
+https://pubsonline.informs.org/toc/isre/10/1
+https://pubsonline.informs.org/toc/isre/9/4
+https://pubsonline.informs.org/toc/isre/9/4
+https://pubsonline.informs.org/toc/isre/9/3
+https://pubsonline.informs.org/toc/isre/9/3
+https://pubsonline.informs.org/toc/isre/9/2
+https://pubsonline.informs.org/toc/isre/9/2
+https://pubsonline.informs.org/toc/isre/9/1
+https://pubsonline.informs.org/toc/isre/9/1
+https://pubsonline.informs.org/toc/isre/8/4
+https://pubsonline.informs.org/toc/isre/8/4
+https://pubsonline.informs.org/toc/isre/8/3
+https://pubsonline.informs.org/toc/isre/8/3
+https://pubsonline.informs.org/toc/isre/8/2
+https://pubsonline.informs.org/toc/isre/8/2
+https://pubsonline.informs.org/toc/isre/8/1
+https://pubsonline.informs.org/toc/isre/8/1
+https://pubsonline.informs.org/toc/isre/7/4
+https://pubsonline.informs.org/toc/isre/7/4
+https://pubsonline.informs.org/toc/isre/7/3
+https://pubsonline.informs.org/toc/isre/7/3
+https://pubsonline.informs.org/toc/isre/7/2
+https://pubsonline.informs.org/toc/isre/7/2
+https://pubsonline.informs.org/toc/isre/7/1
+https://pubsonline.informs.org/toc/isre/7/1
+https://pubsonline.informs.org/toc/isre/6/4
+https://pubsonline.informs.org/toc/isre/6/4
+https://pubsonline.informs.org/toc/isre/6/3
+https://pubsonline.informs.org/toc/isre/6/3
+https://pubsonline.informs.org/toc/isre/6/2
+https://pubsonline.informs.org/toc/isre/6/2
+https://pubsonline.informs.org/toc/isre/6/1
+https://pubsonline.informs.org/toc/isre/6/1
+https://pubsonline.informs.org/toc/isre/5/4
+https://pubsonline.informs.org/toc/isre/5/4
+https://pubsonline.informs.org/toc/isre/5/3
+https://pubsonline.informs.org/toc/isre/5/3
+https://pubsonline.informs.org/toc/isre/5/2
+https://pubsonline.informs.org/toc/isre/5/2
+https://pubsonline.informs.org/toc/isre/5/1
+https://pubsonline.informs.org/toc/isre/5/1
+https://pubsonline.informs.org/toc/isre/4/4
+https://pubsonline.informs.org/toc/isre/4/4
+https://pubsonline.informs.org/toc/isre/4/3
+https://pubsonline.informs.org/toc/isre/4/3
+https://pubsonline.informs.org/toc/isre/4/2
+https://pubsonline.informs.org/toc/isre/4/2
+https://pubsonline.informs.org/toc/isre/4/1
+https://pubsonline.informs.org/toc/isre/4/1
+https://pubsonline.informs.org/toc/isre/3/4
+https://pubsonline.informs.org/toc/isre/3/4
+https://pubsonline.informs.org/toc/isre/3/3
+https://pubsonline.informs.org/toc/isre/3/3
+https://pubsonline.informs.org/toc/isre/3/2
+https://pubsonline.informs.org/toc/isre/3/2
+https://pubsonline.informs.org/toc/isre/3/1
+https://pubsonline.informs.org/toc/isre/3/1
+https://pubsonline.informs.org/toc/isre/2/4
+https://pubsonline.informs.org/toc/isre/2/4
+https://pubsonline.informs.org/toc/isre/2/3
+https://pubsonline.informs.org/toc/isre/2/3
+https://pubsonline.informs.org/toc/isre/2/2
+https://pubsonline.informs.org/toc/isre/2/2
+https://pubsonline.informs.org/toc/isre/2/1
+https://pubsonline.informs.org/toc/isre/2/1
+https://pubsonline.informs.org/toc/isre/1/4
+https://pubsonline.informs.org/toc/isre/1/4
+https://pubsonline.informs.org/toc/isre/1/3
+https://pubsonline.informs.org/toc/isre/1/3
+https://pubsonline.informs.org/toc/isre/1/2
+https://pubsonline.informs.org/toc/isre/1/2
+https://pubsonline.informs.org/toc/isre/1/1
+https://pubsonline.informs.org/toc/isre/1/1
+'''
+# isr 所有链接: 116
+isr_set = set([i for i in isr.split('\n') if i != ''])
+
+import pandas as pd
+# isr当时开了12个线程去抓取 现在把12个线程的信息合并起来
+paper_sys = []     
+col = 'MS'
+for i in range(0, 17):      
+    papers_info = pd.read_pickle('G:/XU_TASK/DTM/%s_%d_paper_infos.pkl' % (col, i))
+    for link in papers_info:
+        items = papers_info[link]
+        for item in items[1:]:
+            paper_sys.append([link, item[0], item[1], item[2]]) # title, publish_time, href
+paper_sys_df = pd.DataFrame(paper_sys)
+paper_sys_df.columns = ['journal_link','title','publish_time','ahref']
+paper_sys_df['pdf_href'] = 'https://pubsonline.informs.org' + paper_sys_df['ahref']
+paper_sys_df['paper_name'] = paper_sys_df['ahref'].apply(lambda x: x.replace('/','_').replace('.','_') + '.pdf' )
+paper_sys_df['paper_path'] = 'G:/XU_TASK/DTM/NEW_PAPERS/%s/' % col + paper_sys_df['paper_name']
+paper_sys_df.index = paper_sys_df['paper_path']
+print(len(set(paper_sys_df['journal_link']))) # isr：116  ms：173 
+# 校验是否所有文章都下载完全 报错则没有下载完全
+if (set(paper_sys_df['journal_link']) - isr_set):
+    raise Exception
+if (isr_set - set(paper_sys_df['journal_link'])):
+    raise Exception
+
+papers_detail_records = pd.read_pickle('G:/XU_TASK/DTM/%spapers_detail_records.pkl' % col)
+papers_detail_df = pd.DataFrame(papers_detail_records, columns = ['journal_t3', 'journal_t2', 'journal_t1', 'journal_link', 'iPageRange'])
+# 校验是否所有文章都下载完全 报错则没有下载完全
+if (set(papers_detail_df['journal_link']) - isr_set):
+    raise Exception
+if (isr_set - set(papers_detail_df['journal_link'])):
+    raise Exception
+
+paper_sys_df = paper_sys_df.merge(papers_detail_df, how='left', on = ['journal_link'])
+pd.to_pickle(paper_sys_df, '%s_download_papers.pkl' % col)
+
+#%%
+import os
+from pdfminer.pdfparser import PDFParser,PDFDocument
+import pandas as pd
+
+def get_FileSize(filePath):
+    '''获取文件的大小,结果保留两位小数，单位为MB'''
+    fsize = os.path.getsize(filePath)
+    fsize = fsize/float(1024*1024)
+    return round(fsize,2)
+
+
+def get_reader(filename):
+    try:
+        fp = open(filename, 'rb')
+        praser = PDFParser(fp)
+        doc = PDFDocument()
+        # 连接分析器 与文档对象
+        praser.set_document(doc)
+        doc.set_parser(praser)
+        fp.close()
+        print('open file successfully')
+        return (True, 'open')
+    except Exception as err:
+        if 'stream with no endstream' in str(err):
+            return (False, repr(err)) # 不完整引起
+        else:
+            return (None, repr(err)) # 'PSEOF()' 文件大小为0引起
+ 
+ # 遍历文件 读取文件夹传输失败的文件
+errors = []
+download = []
+path = 'G:/XU_TASK/DTM/NEW_PAPERS'
+papers =  os.listdir(path + '/%s' % col)
+n = len(papers)
+for paper in papers:
+    paper_path = '/'.join([path + '/%s' % col, paper])
+    flag, error_msg = get_reader(paper_path)
+    if not flag:
+        errors.append([col, paper_path, error_msg])
+errors_df = pd.DataFrame(errors, columns=['journal','paper_path','error_msg'])
+print('error_rate: ', len(errors_df)/n)
+
+# 比较实际论文数量是否与理论下载数量相同
+n_ = 0
+for i in range(0, 17):      
+    papers_info = pd.read_pickle('G:/XU_TASK/DTM/%s_%d_paper_infos.pkl' % (col, i))
+    for link in papers_info:
+        items = papers_info[link]
+        if items[0]==0:
+            raise
+        n_ = n_ + items[0]
+if n != n_:
+    raise
+    
+paper_sys_df.index = paper_sys_df['paper_path']
+errors_df.index = errors_df['paper_path']
+paper_sys_df['error_msg'] = None
+paper_sys_df.loc[errors_df['paper_path'], 'error_msg'] = errors_df['error_msg']
+
+error_ = paper_sys_df.loc[errors_df['paper_path']]
+pd.to_pickle(error_, 'MS_check.pkl')
+
+reason = pd.read_pickle('H:/MS_1_html.pkl')
+reason = pd.DataFrame(reason).T
+reason['paper_path'] = reason.index
+reason['paper_path'] = reason['paper_path'].apply(lambda x: 'G:/XU_TASK/DTM/NEW_PAPERS/%s/' % col  + x.split('/')[-1])
+# 校验是否没下载成功的论文原因都是加锁了 也就是页面有title但是没办法下载 无权限
+if (set(reason['paper_path']) - set(errors_df['paper_path'])):
+    raise Exception
+if (set(errors_df['paper_path']) - set(reason['paper_path'])):
+    raise Exception
+paper_sys_df = paper_sys_df.merge(reason, how='left', on=['paper_path'])
+# 检查是否有没有失误没下载下来的 
+print(sum((paper_sys_df['flag'].isnull())&(paper_sys_df['error_msg'].notnull())))
+pd.to_pickle(paper_sys_df, '%s_download_papers.pkl' % col)
+
+
+tmp2 = pd.read_pickle('%s_download_papers.pkl' % col)
+paper_sys_df.index = paper_sys_df['paper_path']
+paper_error = paper_sys_df.loc[errors_df['paper_path']]
+print(paper_error['publish_time'].value_counts())
+paper_sys_df['journal_year'] = paper_sys_df['journal_t3'].apply(lambda x: x.split(' ')[-1])
+paper_sys_df['flag'] = paper_sys_df['flag'].fillna(0)
+tmp2 = paper_sys_df[paper_sys_df['flag'] ==True].groupby(['journal_year'])[['flag']].count()
+tmp1 = paper_sys_df[paper_sys_df['flag'] ==0].groupby(['journal_year'])[['flag']].count()
+tmp1 = pd.merge(tmp1,tmp2, how='outer', left_index=True, right_index=True)
+tmp1.columns = ['success','fail']
+tmp1 = tmp1.fillna(0)
+
+
+
+# 随机检查是否下载完全
+tmp = paper_sys_df[paper_sys_df['journal_link']=='https://pubsonline.informs.org/toc/isre/29/1']
+
+
+
+
+
+
+
+
+
+
+
